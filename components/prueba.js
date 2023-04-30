ea922f1aaebd8cbb945feab10d285c62c7816913
adrian <adriancasanova_@outlook.es>
2023-04-30 15:41:40 -0300

    primer commit

---
+++ components/prueba.js
@@ -0,0 +1,11 @@
+ const suma =  ((a, b) => {   
+    return a + b;
+   // const c = a + b;
+   // return c;
+   })
+   export default suma;
+
+ /*  const deleteTask = (event) => {
+    const parent = event.target.parentElement;
+    parent.remove();
+  }; */
